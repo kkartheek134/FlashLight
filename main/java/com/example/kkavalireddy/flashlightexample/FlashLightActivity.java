52f52e54de061db0adb5371b9f886e65467cd9cd
Kartheek <33369524+kkartheek134@users.noreply.github.com>
2017-12-12 18:16:49 +0530

    Add files via upload

---
+++ main/java/com/example/kkavalireddy/flashlightexample/FlashLightActivity.java
@@ -0,0 +1,189 @@
+package com.example.kkavalireddy.flashlightexample;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class FlashLightActivity extends AppCompatActivity {
+
+    private ImageButton btnSwitch;
+    private Camera camera;
+    private boolean isFlashOn;
+    private boolean hasFlash;
+    private Camera.Parameters params;
+    private MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_light);
+
+        btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
+
+        hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!hasFlash) {
+            // device doesn't support flash
+            // Show alert message and close the application
+            AlertDialog alert = new AlertDialog.Builder(FlashLightActivity.this)
+                    .create();
+            alert.setTitle("Error");
+            alert.setMessage("Sorry, your device doesn't support flash light!");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+        }
+
+        getCamera();
+
+        toggleButtonImage();
+
+        btnSwitch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (isFlashOn) {
+                    // turn off flash
+                    turnOffFlash();
+                } else {
+                    // turn on flash
+                    turnOnFlash();
+                }
+            }
+        });
+    }
+
+    private void turnOnFlash() {
+        if (!isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            // play sound
+            playSound();
+
+            params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview();
+            isFlashOn = true;
+
+            // changing button/switch image
+            toggleButtonImage();
+        }
+
+    }
+
+    private void toggleButtonImage() {
+        if (isFlashOn) {
+            btnSwitch.setImageResource(R.drawable.btn_switch_on);
+        } else {
+            btnSwitch.setImageResource(R.drawable.btn_switch_off);
+        }
+    }
+
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                params = camera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("Camera Error. Failed to Open ", e.getMessage());
+            }
+        }
+
+    }
+
+    private void turnOffFlash() {
+        if (isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            // play sound
+            playSound();
+
+            params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(params);
+            camera.stopPreview();
+            isFlashOn = false;
+
+            // changing button/switch image
+            toggleButtonImage();
+        }
+    }
+
+    private void playSound() {
+        if (isFlashOn) {
+            mp = MediaPlayer.create(FlashLightActivity.this, R.raw.light_switch_off);
+        } else {
+            mp = MediaPlayer.create(FlashLightActivity.this, R.raw.light_switch_on);
+        }
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // TODO Auto-generated method stub
+                mp.release();
+            }
+        });
+        mp.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // on pause turn off the flash
+        turnOffFlash();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // on resume turn on the flash
+        if (hasFlash)
+            turnOnFlash();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // on starting the app get the camera params
+        getCamera();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // on stop release the camera
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+}
